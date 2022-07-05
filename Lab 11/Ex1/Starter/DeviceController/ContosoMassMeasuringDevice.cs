7c4e0a0e9a31f0fccb5b99e0b2d6a2e844b8e08e
Иван Александрович Дрей <83556084+dreyvania999@users.noreply.github.com>
2022-07-05 18:36:45 +0300

    Add files via upload

---
+++ Lab 11/Ex1/Starter/DeviceController/ContosoMassMeasuringDevice.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+
+using DeviceControl;
+
+namespace Contoso.MeasuringDevices
+{
+    class MassMeasuringDevice : IControllableDevice
+    {
+        Random rnd;
+        public MassMeasuringDevice()
+        {
+            rnd = new Random();
+        }
+        public void StartDevice()
+        {
+        }
+
+        public void StopDevice()
+        {
+        }
+        public int GetLatestMeasure()
+        {
+            Thread.Sleep(rnd.Next(5000));
+            return rnd.Next(1390);
+        }
+    }
+}
---
+++ Lab 11/Ex1/Starter/DeviceController/FabrikamLengthMeasuringDevice.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+
+using DeviceControl;
+
+namespace Fabrikam.Devices.MeasuringDevices
+{
+    class LengthMeasuringDevice : IControllableDevice
+    {
+        Random rnd;
+        public LengthMeasuringDevice()
+        {
+            rnd = new Random();
+        }
+
+        public void StartDevice()
+        {
+                
+        }
+
+        public void StopDevice()
+        {
+        }
+
+        public int GetLatestMeasure()
+        {
+            Thread.Sleep(rnd.Next(6000));
+            return rnd.Next(1000);
+        }
+    }
+}
