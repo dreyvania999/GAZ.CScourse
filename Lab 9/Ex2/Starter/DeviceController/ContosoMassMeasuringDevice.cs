0c9eb939beddf86af2109f66b184f30248c94881
Иван Александрович Дрей <83556084+dreyvania999@users.noreply.github.com>
2022-07-05 18:35:10 +0300

    Add files via upload

---
+++ Lab 9/Ex2/Starter/DeviceController/ContosoMassMeasuringDevice.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using DeviceControl;
+
+namespace Contoso.MeasuringDevices
+{
+    class MassMeasuringDevice : IControllableDevice
+    {
+        Random rnd;
+
+        public MassMeasuringDevice()
+        {
+            rnd = new Random();
+        }
+
+        public void StartDevice()
+        {
+           
+        }
+        public void StopDevice()
+        {
+            
+        }
+
+        public int GetLatestMeasure()
+        {
+            return rnd.Next(1390);
+        }
+    }
+}
